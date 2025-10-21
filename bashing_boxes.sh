d68a67d0f26dd0ecd9bd6c3264b7f8fb7df9cb1d
Chinae <Chinaenwankwo@gmail.com>
2025-10-21 10:38:20 -0500

    Just finished all functions

--- bashing_boxes.sh
+++ bashing_boxes.sh
@@ -11,9 +11,10 @@ echo "You have five different options"
 wait 1
 echo "option 1 |Print list"
 echo "option 2 |Print item at X position in list"
+echo "option 3 |Add item to list"
 echo "option 3 |Add item to list remove last item from list"
 echo "option 4 |remove item - from X position"
-echo "option 5 |Exit"
+echo "option 6 |Exit"
 
 read -p "Choose 1 option from 1 to 5"
 
@@ -30,11 +31,31 @@ local item+=($answer)
 }
 
 
+printitemfromlist() {
+read -p "What would you like to print from the list ${array[@]}" answer3
+echo "$answer3"
+
+}
+
+Additem_Remove()
+echo "When you add a item to the list, the last item will be removed"
+read -p "what would you like to add" answer4
+local item1+=($answer4)
+unset ${array[index10]}
+echo ${array[index10]} "clock tower has been deleted and $answer4 has been added"
+
+
+
 
 Removeitem() {
 read -p "Which Item would you like to remove from the list" answer1
 unset ${array[$index]}
 echo ${array[$index]} "The $index has been removed from the list"
+}
+
+Leavegame() {
+	echo "leaving the game"; then
+		exit
 }
 
 
